9fc5e47a3de7251446184a63b826d2e6b968a951
Maheshwaran00 <maheshwaran0462@gmail.com>
2024-01-04 12:43:17 +0530

    changes are committed

--- tests/demosites.spec.ts
+++ tests/demosites.spec.ts
@@ -4,27 +4,27 @@ test.use({ disableAutoCapture: true,resourceArchiveTimeout:1000*60 });
 
 test("Demoblaze", async ({ page },testInfo) => {
     
-    await page.goto("https://www.demoblaze.com/");
+    // await page.goto("https://www.demoblaze.com/");
 
-    await expect(page).toHaveTitle("STORE");
+    // await expect(page).toHaveTitle("STORE");
 
-    await page.locator('//span[text()="Next"]').nth(0).click();
+    // await page.locator('//span[text()="Next"]').nth(0).click();
 
-    await takeArchive(page, "First slide", testInfo);
+    // await takeArchive(page, "First slide", testInfo);
 
-    await page.locator('//span[text()="Next"]').nth(0).click();
+    // await page.locator('//span[text()="Next"]').nth(0).click();
 
-    await takeArchive(page, "Second slide", testInfo);
+    // await takeArchive(page, "Second slide", testInfo);
 
-    await page.locator('[id="itemc"]').nth(0).click();
+    // await page.locator('[id="itemc"]').nth(0).click();
 
-    await page.waitForTimeout(1000);
+    // await page.waitForTimeout(1000);
  
-    await takeArchive(page, "Phones Page", testInfo);
+    // await takeArchive(page, "Phones Page", testInfo);
 
-    await page.locator('[class="card-block"]').nth(0).click();
+    // await page.locator('[class="card-block"]').nth(0).click();
 
-    await takeArchive(page, "Samsung Page", testInfo);
+    // await takeArchive(page, "Samsung Page", testInfo);
 
     await page.goto("https://automationbookstore.dev/");
    
