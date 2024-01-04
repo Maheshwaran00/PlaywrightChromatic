2c16caa0a461f7279fabdce7ff930f3d6a2e6bd8
Maheshwaran00 <maheshwaran0462@gmail.com>
2024-01-04 12:25:25 +0530

    changes are committed

--- tests/demosites.spec.ts
+++ tests/demosites.spec.ts
@@ -4,15 +4,15 @@ test.use({ disableAutoCapture: true,resourceArchiveTimeout:1000*60 });
 
 test("Demoblaze", async ({ page },testInfo) => {
     
-     await page.goto("https://www.demoblaze.com/");
+    await page.goto("https://www.demoblaze.com/");
 
-     await expect(page).toHaveTitle("STORE");
+    await expect(page).toHaveTitle("STORE");
 
-     await page.locator('//span[text()="Next"]').nth(0).click();
+    await page.locator('//span[text()="Next"]').nth(0).click();
 
-     await takeArchive(page, "First slide", testInfo);
+    await takeArchive(page, "First slide", testInfo);
 
-     await page.locator('//span[text()="Next"]').nth(0).click();
+    await page.locator('//span[text()="Next"]').nth(0).click();
 
     await takeArchive(page, "Second slide", testInfo);
 
